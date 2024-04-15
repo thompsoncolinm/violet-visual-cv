5d1d8bfb990ba0e66c7af20683ef1f4520dd2809
Colin Thompson <thompsoncolinm@gmail.com>
2024-04-15 10:18:54 -0400

    separated sections

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,25 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+			'xs': '375px',
+			
+			'xs_only': {'min': '375px', 'max': '575px'},
+
+			'sm': '576px',
+
+			'sm_only': {'min': '576px', 'max': '959px'},
+	  
+			'md': '960px',
+	  
+			'md_only': {'min': '960px', 'max': '1439px'},
+
+			'lg': '1440px',
+			
+			'lg_only': {'min': '1440px', 'max': '1536px'},
+	  
+			'xl': '1536px',
+		},
 		fontSize: {
 			h1: '3rem'
 		},
@@ -14,7 +33,7 @@ export default {
 				},
 			},
 			fontFamily: {
-				body: ['Nunito'],
+				body: ['Roboto'],
 				display: ['Roboto'],
 			},
 			fontSize: {
