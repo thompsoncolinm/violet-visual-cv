c2163286cf4869377e5ffd366833f5d9739e0f3f
Colin Thompson <thompsoncolinm@gmail.com>
2024-04-29 20:57:29 +0000

    added starter code

--- src/scripts/nav-toggle.js
+++ src/scripts/nav-toggle.js
@@ -1,7 +1,9 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.nav-menu');
 
-navToggle.addEventListener('click', () => {
-    navToggle.classList.toggle('before:hidden');
-    navMenu.classList.toggle('hidden');
-});
+if (navToggle && navMenu) {
+    navToggle.addEventListener('click', () => {
+        navToggle.classList.toggle('before:hidden');
+        navMenu.classList.toggle('hidden');
+    });
+}
