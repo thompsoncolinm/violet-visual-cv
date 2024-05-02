d38b56b323264e13a424bb2960977235fd15f070
Colin Thompson <thompsoncolinm@gmail.com>
2024-05-02 11:48:24 -0400

    tried using hybrid output

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server', // or 'hybrid'
+  output: 'hybrid', // 'server' or 'hybrid'
   adapter: vercel(),
 });
